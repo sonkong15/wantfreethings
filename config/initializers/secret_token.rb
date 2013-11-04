efb3e58f93bdccc6c20b66a0a95a0bdf9a973b5e
Donnell A Rawlings <donnierawlings@gmail.com>
2013-11-03 21:01:54 -0500

    lets do this

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wantfreethings::Application.config.secret_token = 'c57e6e5e09a5520b13a449dc4c240b678a88e7f9ac278300e03a5cca6c8847a4aa3eee4d45e426d6294ab5c7c02f376a033a48a2be57dca1026590ac4ccb02e2'
